ada24b95c43a61ae949b46d21540c6ffb421c28d
Bharathy <bharathy_nats@yahoo.com>
2018-02-05 18:32:40 -0600

    add routes for metadata

---
+++ bio_flask.py
@@ -0,0 +1,56 @@
+import os
+import pandas as pd
+from flask import Flask, jsonify, render_template
+
+# read the csv files into pandas as dataframes
+filepath1 = os.path.join("belly_button_biodiversity_samples.csv")
+samples_df = pd.read_csv(filepath1)
+
+samples = list(samples_df.columns.values)
+# read the csv files into pandas as dataframes
+filepath2 = os.path.join("belly_button_biodiversity_otu_id.csv")
+otu_df = pd.read_csv(filepath2)
+otu_description = list(otu_df.lowest_taxonomic_unit_found)# read the csv files into pandas as dataframes
+filepath3 = os.path.join("belly_button_biodiversity_MetaData.csv")
+metaData_df = pd.read_csv(filepath3)
+
+sample_metaData = metaData_df.to_dict('records')
+print(sample_metaData)
+
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/names")
+def names():
+    return jsonify(samples)
+
+
+@app.route("/otu")
+def otu():
+    return jsonify(otu_description)
+
+@app.route("/metadata/<sample>")
+def metaData(sample):
+    
+    """Fetch the metadata for sample whose sample matches
+       the path variable supplied by the user, or a 404 if not."""
+    sample_id=int(sample)
+    for each in sample_metaData:
+        search_term = each['SAMPLEID']
+        print(search_term,sample_id)
+        if search_term == sample_id:
+            return jsonify(each)
+        message = "Sample ID  " + sample + " not found."
+        return jsonify({"error": message}), 404
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
+
+
+
